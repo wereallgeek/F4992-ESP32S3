c18a26b93e6f48d864443095e9e10e0c67c114ff
We're all geeks <wereallgeeks@hotmail.com>
2026-04-13 21:37:26 -0400

    recolorization
    changed colors of configuration tabs.
    
    removed this horrendous orange

--- F4992-ESP32/ESPUI.ino
+++ F4992-ESP32/ESPUI.ino
@@ -23,7 +23,7 @@ void espui_init() {
 
   //Tonearm Status-----------------------------------------------------------------------------------------------------------------
   auto tonearmtab = ESPUI.addControl(Tab, "", "Status");
-  armStatusLabelId = ESPUI.addControl(Label, "Status", "status", Carrot, tonearmtab, textCallback);
+  armStatusLabelId = ESPUI.addControl(Label, "Status", "status", Dark, tonearmtab, textCallback);
   armPositionLabelId = ESPUI.addControl(Text, "", "position", None, armStatusLabelId, textCallback);
   //Tonearm Status-----------------------------------------------------------------------------------------------------------------
 
@@ -38,25 +38,25 @@ void espui_init() {
   
   //Turntable configuration--------------------------------------------------------------------------------------------------------
   auto configtab = ESPUI.addControl(Tab, "", "Configuration");
-  device_name_text = ESPUI.addControl(Text, "Device name", stored_devicename, Carrot, configtab, textCallback);
+  device_name_text = ESPUI.addControl(Text, "Device name", stored_devicename, Dark, configtab, textCallback);
   highVerbosity_switch = ESPUI.addControl(Switcher, "High Verbosity", String(highVerbosity), Dark, configtab, verbosityCallback);
   auto configsave = ESPUI.addControl(Button, "Save", "Save", Peterriver, configtab, SaveTurntableDetailsCallback);
   auto espreboot = ESPUI.addControl(Button, "", "Reboot ESP", None, configsave, ESPReset);
 
   //WiFi---------------------------------------------------------------------------------------------------------------------------
   auto wifitab = ESPUI.addControl(Tab, "", "WiFi");
-  wifi_ssid_text = ESPUI.addControl(Text, "SSID", stored_ssid, Carrot, wifitab, textCallback);
-  wifi_pass_text = ESPUI.addControl(Text, "Password", stored_pass, Carrot, wifitab, textCallback);
+  wifi_ssid_text = ESPUI.addControl(Text, "SSID", stored_ssid, Dark, wifitab, textCallback);
+  wifi_pass_text = ESPUI.addControl(Text, "Password", stored_pass, Dark, wifitab, textCallback);
   ESPUI.setInputType(wifi_pass_text, "password");
   ESPUI.addControl(Max, "", "32", None, wifi_ssid_text);
   ESPUI.addControl(Max, "", "64", None, wifi_pass_text);
-  mqtt_server_text = ESPUI.addControl(Text, "MQTT server", stored_mqtt_server, Carrot, wifitab, textCallback);
-  mqtt_user_text = ESPUI.addControl(Text, "MQTT user", stored_mqtt_user, Carrot, wifitab, textCallback);
-  mqtt_pass_text = ESPUI.addControl(Text, "MQTT password", stored_mqtt_pass, Carrot, wifitab, textCallback);
+  mqtt_server_text = ESPUI.addControl(Text, "MQTT server", stored_mqtt_server, Dark, wifitab, textCallback);
+  mqtt_user_text = ESPUI.addControl(Text, "MQTT user", stored_mqtt_user, Dark, wifitab, textCallback);
+  mqtt_pass_text = ESPUI.addControl(Text, "MQTT password", stored_mqtt_pass, Dark, wifitab, textCallback);
   ESPUI.setInputType(mqtt_pass_text, "password");
-  mqtt_enabled_switch = ESPUI.addControl(Switcher, "Enable MQTT", String(mqtt_enabled), Carrot, wifitab, textCallback);
-  mqtt_topic_in_text = ESPUI.addControl(Text, "MQTT topic IN", stored_mqtt_topic_in, Carrot, wifitab, textCallback);
-  mqtt_topic_out_text = ESPUI.addControl(Text, "MQTT topic OUT", stored_mqtt_topic_out, Carrot, wifitab, textCallback);
+  mqtt_enabled_switch = ESPUI.addControl(Switcher, "Enable MQTT", String(mqtt_enabled), Dark, wifitab, textCallback);
+  mqtt_topic_in_text = ESPUI.addControl(Text, "MQTT topic IN", stored_mqtt_topic_in, Dark, wifitab, textCallback);
+  mqtt_topic_out_text = ESPUI.addControl(Text, "MQTT topic OUT", stored_mqtt_topic_out, Dark, wifitab, textCallback);
 
   auto wifisave = ESPUI.addControl(Button, "Save", "Save", Peterriver, wifitab, SaveWifiDetailsCallback);
   auto espreset = ESPUI.addControl(Button, "", "Reboot ESP", None, wifisave, ESPReset);
